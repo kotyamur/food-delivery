577a6ec4209aea46fc7af08e75d8651c9ad7f210
kotyamur <99125819+kotyamur@users.noreply.github.com>
2023-05-27 16:23:08 +0000

    Deploying to gh-pages from @ kotyamur/food-delivery@9cf5c634abd99e03463472ac615b63f1d3754ffc 🚀

---
+++ static/js/574.aadb7dac.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[574],{574:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Cart"})}}}]);
+//# sourceMappingURL=574.aadb7dac.chunk.js.map

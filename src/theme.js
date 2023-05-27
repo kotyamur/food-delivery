207f91c3b9c99432fccdc2e4c4179e01fad7094a
Kateryna Kleshch <ketrins1993@gmail.com>
2023-05-27 18:22:50 +0300

    add styles

---
+++ src/theme.js
@@ -0,0 +1,45 @@
+export const theme = {
+  colors: {
+    black: '#010101',
+    white: '#fff',
+    green: '#4caf50',
+    orange: '#ffa500',
+    blue: '#2196f3',
+    secondaryText: '#757575',
+    primaryText: '#212121',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    ml: '28px',
+    l: '32px',
+    xl: '48px',
+  },
+  fontWeights: {
+    normal: 400,
+    semiBold: 500,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+};
